b1590f8b9550f2ad6870bbec003c02f8e42408a9
SatohAyaka <sayaka310ss@docomo.ne.jp>
2025-04-22 12:48:28 +0900

    add: 滞在者情報を元の形式のまま取得するファイル、型の追加

---
+++ src/features/getStayersID/getStayersData.tsx
@@ -0,0 +1,16 @@
+const BASE_URL = process.env.REACT_APP_STAY_WATCH_URL;
+const GET_STAYERS_API = '/api/v1/stayers';
+
+import Stayer from "../../types/Stayer";
+
+export async function getStayers(): Promise<Stayer[]> {
+    const response = await fetch(`${BASE_URL}${GET_STAYERS_API}`);
+
+    if (!response.ok) {
+        throw new Error(`APIリクエスト失敗: ${response.status}`);
+    }
+
+    const data: Stayer[] = await response.json();
+    console.log(data);
+    return data;
+}
---
+++ src/types/Stayer.ts
@@ -0,0 +1,14 @@
+type Stayer = {
+    id: number;
+    name: string;
+    room: string;
+    roomId: number;
+    tags: [
+        {
+            id: number;
+            name: string;
+        },
+    ];
+};
+
+export default Stayer;

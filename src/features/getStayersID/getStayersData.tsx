f09e15e9bcd4f1202d0ec12966bac1e2be1292ea
SatohAyaka <sayaka310ss@docomo.ne.jp>
2025-04-22 17:49:15 +0900

    fix: 返り値をidのみに変更

--- src/features/getStayersID/getStayersData.tsx
+++ src/features/getStayersID/getStayersData.tsx
@@ -1,16 +1,16 @@
 const BASE_URL = process.env.REACT_APP_STAY_WATCH_URL;
-const GET_STAYERS_API = '/api/v1/stayers';
+const GET_STAYERS = process.env.REACT_APP_STAYERS_API;
 
 import Stayer from "../../types/Stayer";
 
-export async function getStayers(): Promise<Stayer[]> {
-    const response = await fetch(`${BASE_URL}${GET_STAYERS_API}`);
+export async function getStayers(): Promise<number[]> {
+    const response = await fetch(`${BASE_URL}${GET_STAYERS}`);
 
     if (!response.ok) {
         throw new Error(`滞在者情報取得失敗: ${response.status}`);
     }
 
-    const data: Stayer[] = await response.json();
-    console.log(data);
-    return data;
+    const stayersData: Stayer[] = await response.json();
+    const userId = stayersData.map((stayer) => stayer.id)
+    return userId;
 }

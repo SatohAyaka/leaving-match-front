7e8c5f9c2ea9cef6a0206dcb30715707642c090d
SatohAyaka <sayaka310ss@docomo.ne.jp>
2025-06-03 18:06:34 +0900

    add: 区間抽出をする機能の追加

---
+++ src/features/recommendedDepartureTime/getSection.ts
@@ -0,0 +1,28 @@
+import { Prediction } from "@/src/types/Prediction";
+import { stringTimeToNumber } from "./stringTimeToNumber";
+
+export const findMaxCountInterval = (prediction: Prediction[]): [number, number, number] => {
+    const width = 30;
+    const predictionTimes = prediction.map((prediction) => (prediction.predictionTime));
+    // 時刻をstring→number（hour*60+minute）
+    const timeToNumber: number[] = predictionTimes.map(stringTimeToNumber
+    ).sort((a, b) => a - b);
+    let maxCount = 0;
+    let bestStart = 0;
+    let left = 0;
+
+
+    for (let right = 0; right < timeToNumber.length; right++) {
+        while (timeToNumber[right] - timeToNumber[left] >= width) {
+            left++;
+        }
+        const count = right - left + 1;
+        if (count > maxCount) {
+            maxCount = count;
+            bestStart = timeToNumber[left];
+        }
+    }
+    return [
+        bestStart, bestStart + width, maxCount,
+    ];
+}

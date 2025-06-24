e4aa83be7b74cfa9dbb72a4d0cc6b3c9e8feca78
SatohAyaka <sayaka310ss@docomo.ne.jp>
2025-06-24 17:52:54 +0900

    fix: 表示を変更

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
-import PredictionTimes from "@/src/features/getPrediction/components/PredictionTimes";
+// import PredictionTimes from "@/src/features/getPrediction/components/PredictionTimes";
+import DisplayTime from "../features/getPrediction/components/home";
+// import PredictionTimes from "../features/getPrediction/components/PredictionTimes";
 
 // ホーム
 const Home = () => {
-  return <PredictionTimes />;
+  return <DisplayTime />;
+  // return <PredictionTimes />
 };
 
 export default Home;

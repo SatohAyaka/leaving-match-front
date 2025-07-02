42459c9e56fb0aa4ed52c3a9ea8b4c61bba9fc9a
SatohAyaka <sayaka310ss@docomo.ne.jp>
2025-07-02 11:35:27 +0900

    fix: ディレクトリ構成変更による呼び出しの変更

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 // import PredictionTimes from "@/src/features/getPrediction/components/PredictionTimes";
 import DisplayTime from "../features/components/home";
-// import PredictionTimes from "../features/getPrediction/components/PredictionTimes";
+
+// import PredictionTimes from "../features/PredictionTimes";
+
 
 // ホーム
 const Home = () => {

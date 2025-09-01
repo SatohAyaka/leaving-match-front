5d1281d82257c2234eb22cf2e2f1762127648cf1
SatohAyaka <sayaka310ss@docomo.ne.jp>
2025-09-02 03:09:02 +0900

    add: 投票結果表示画面

---
+++ src/pages/resultDisplay.tsx
@@ -0,0 +1,41 @@
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import { numberTimeToString } from "../features/utils/recommendedDepartureTime/numberTimeToString";
+import { getLatestResult } from "../features/utils/result/getLatestResult";
+import { Result } from "../types/Result";
+
+export function ResultDisplay() {
+    const router = useRouter();
+    const [time, setTime] = useState<string | null>(null);
+    const [member, setMember] = useState<number | null>(null);
+
+    useEffect(() => {
+        if (!router.isReady) return;
+        const fetchResult = async () => {
+            try {
+                const resultData: Result = await getLatestResult();
+                setTime(numberTimeToString(resultData.Bustime));
+                setMember(resultData.Member);
+            } catch (err) {
+                console.error(err);
+                router.push("/home");
+            }
+        };
+        fetchResult();
+    }, [router]);
+
+    return (
+        <body>
+            <div className={`display night`}>
+                <div className="balloon">
+                    {member !== null ? `${member}人くらいと` : "数人と"}<br />
+                    一緒に帰れるかも？
+                </div>
+                <div className="center-box">
+                    <div className="time">{time}</div>
+                    <div className="message">のバスに乗りませんか？</div>
+                </div>
+            </div>
+        </body>
+    );
+}

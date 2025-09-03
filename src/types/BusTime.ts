ed4f4f87534fab7e2bfe0ad96311e2ea7fb45d21
SatohAyaka <sayaka310ss@docomo.ne.jp>
2025-09-03 14:39:40 +0900

    add: 型の追加

---
+++ src/types/BusTime.ts
@@ -0,0 +1,23 @@
+export type SelectBusTime = {
+    previousTime: number;
+    nearestTime: number;
+    nextTime: number;
+}
+
+export type ConvertBusTime = {
+    bustimeId: number;
+    previousTime: string;
+    nearestTime: string;
+    nextTime: string;
+    endTime: string
+};
+
+export type ResponseBustimeData = {
+    BusTimeId: number;
+    RecommendedId: number;
+    PreviousTime: string;
+    NearestTime: string;
+    NextTime: string;
+    CreatedDate: string;
+    EndTime: string;
+}

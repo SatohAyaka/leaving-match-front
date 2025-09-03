ad70f6abd696fcd60e7a6826b0d79bc711a3f67e
SatohAyaka <sayaka310ss@docomo.ne.jp>
2025-09-03 14:38:56 +0900

    fix: 細々ミスの修正

--- src/pages/api/bustime/[id].ts
+++ src/pages/api/bustime/[id].ts
@@ -1,12 +1,17 @@
 // pages/api/bustime/[id].ts
 
+import { ConvertBusTime, ResponseBustimeData } from '@/src/types/BusTime';
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 const BASE_URL = process.env.LEAVING_MATCH_API;
 const ENDPOINT = process.env.LEAVING_MATCH_BUSTIME;
 
+function toDateSafe(isoString: string): Date {
+    return new Date(isoString.replace("+09:00", "Z"));
+}
+
 async function postBustimeHandler(req: NextApiRequest, res: NextApiResponse) {
-    if (req.method !== "GET") {
+    if (req.method !== "POST") {
         return res.status(405).json({ error: "Method Not Allowed" });
     }
     if (!BASE_URL || !ENDPOINT) {
@@ -56,19 +61,19 @@ async function getBusTimeHandler(req: NextApiRequest, res: NextApiResponse) {
     }
     const safeBustimeId = String(Number(id));
 
-    const apiUrl = new URL(`${ENDPOINT}/${safeBustimeId}`, BASE_URL).toString();
+    const apiUrl = new URL(`${BASE_URL}${ENDPOINT}/${safeBustimeId}`);
     try {
         const response = await fetch(apiUrl);
         if (!response.ok) {
             return res.status(response.status).json({ error: "外部API呼び出しに失敗しました" });
         }
-        const data = await response.json();
-        const converted = {
-            bustimeId: data.busTimeId,
-            previousTime: new Date(data.previousTime).toISOString().substring(11, 16),
-            nearestTime: new Date(data.nearestTime).toISOString().substring(11, 16),
-            nextTime: new Date(data.nextTime).toISOString().substring(11, 16),
-            endTime: new Date(data.createdDate).toISOString().substring(11, 16),
+        const data: ResponseBustimeData[] = await response.json();
+        const converted: ConvertBusTime = {
+            bustimeId: data[0].BusTimeId,
+            previousTime: toDateSafe(data[0].PreviousTime).toISOString().substring(11, 16),
+            nearestTime: toDateSafe(data[0].NearestTime).toISOString().substring(11, 16),
+            nextTime: toDateSafe(data[0].NextTime).toISOString().substring(11, 16),
+            endTime: toDateSafe(data[0].EndTime).toISOString().substring(11, 16),
         };
         return res.status(200).json(converted);
     } catch (err) {

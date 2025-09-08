e65c18f3591f88032cd09193ddd1ffd523805eb5
SatohAyaka <sayaka310ss@docomo.ne.jp>
2025-09-08 20:59:20 +0900

    fix: 現在時刻より後のものを取り除いた

--- src/features/utils/recommendedDepartureTime/timeSort.ts
+++ src/features/utils/recommendedDepartureTime/timeSort.ts
@@ -1,6 +1,12 @@
 import { usePrediction } from "@/src/types/Prediction";
 
 export const timeSort = (prediction: usePrediction[]) => {
-    const predictionTimes = prediction.map((prediction) => (prediction.predictionTime)).sort((a, b) => a - b);
+    const now = new Date();
+    const currentMinutes = now.getHours() * 60 + now.getMinutes();
+
+    const predictionTimes = prediction
+        .map((prediction) => (prediction.predictionTime))
+        .filter(time => time >= currentMinutes)
+        .sort((a, b) => a - b);
     return predictionTimes;
 }

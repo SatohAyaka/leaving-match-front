95b775d87df6b5da7dc43b3f8c8bda9bde89e364
SatohAyaka <sayaka310ss@docomo.ne.jp>
2025-09-11 00:25:12 +0900

    fix: サーバーコンポーネント呼び出し部分修正

--- src/features/utils/bustime/postBustime.ts
+++ src/features/utils/bustime/postBustime.ts
@@ -5,7 +5,7 @@ export async function postBustime(recommendedId: number, selectBustime: SelectBu
     const nearest = selectBustime.nearestTime;
     const next = selectBustime.nextTime;
 
-    const response = await fetch(`api/bustime/${recommendedId}?previous=${previous}&nearest=${nearest}&next=${next}`, {
+    const response = await fetch(`/api/bustime/${recommendedId}?previous=${previous}&nearest=${nearest}&next=${next}`, {
         method: "POST",
     });
     if (!response.ok) {
--- src/features/utils/result/getLatestResult.ts
+++ src/features/utils/result/getLatestResult.ts
@@ -1,12 +1,12 @@
 import { Result } from "@/src/types/Result";
 
 export async function getLatestResult(): Promise<Result> {
-    const response = await fetch(`api/bustime/latest`);
+    const response = await fetch(`/api/bustime/latest`);
     if (!response.ok) {
         throw new Error(`BustimeId取得失敗: ${response.status}`);
     }
     const bustimeId: number = await response.json();
-    const resultResponse = await fetch(`api/result/${bustimeId}`);
+    const resultResponse = await fetch(`/api/result/${bustimeId}`);
     if (!resultResponse.ok) {
         throw new Error(`Result_Data取得失敗: ${resultResponse.status}`);
     }

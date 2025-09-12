3e2b2fbd87143ad6315ed1f726c9b2ba1c1e3046
SatohAyaka <sayaka310ss@docomo.ne.jp>
2025-09-12 14:28:40 +0900

    add: post result handler

---
+++ src/features/utils/result/postResult.ts
@@ -0,0 +1,10 @@
+export async function postResult(bustimeId: number) {
+    const response = await fetch(`api/result/${bustimeId}`, {
+        method: "POST",
+    });
+    if (!response.ok) {
+        throw new Error(`Result登録失敗: ${response.status}`);
+    }
+    const resultId: number = await response.json();
+    return resultId;
+}

4f1ed150e415d8d8d860b0aa2fc9390e3bc1f24a
SatohAyaka <sayaka310ss@docomo.ne.jp>
2025-10-05 19:42:52 +0900

    refactor: vote

--- src/features/utils/vote/getCurrentVote.ts
+++
@@ -1,8 +0,0 @@
-import { Vote } from "@/src/types/Vote";
-import { fetchApi } from "../../lib/fetchApi";
-
-export async function getCurrentVote(): Promise<Vote> {
-    const bustimeId: number = await fetchApi<number>(`/api/bustime/latest`);
-    const voteData: Vote = await fetchApi<Vote>(`/api/vote/${bustimeId}`);
-    return voteData;
-}

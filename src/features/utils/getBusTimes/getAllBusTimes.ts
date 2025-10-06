43e0d8e59d066cf9fa144ddf18c2cd760226ba1b
SatohAyaka <sayaka310ss@docomo.ne.jp>
2025-10-06 13:03:10 +0900

    refactor: bus API

--- src/features/utils/getBusTimes/getAllBusTimes.ts
+++
@@ -1,8 +0,0 @@
-import { BusTime } from "@/src/types/Bus";
-import { fetchApi } from "../../lib/fetchApi";
-
-export async function GetBusTime(): Promise<BusTime[]> {
-    const allTimes = await fetchApi<BusTime[]>(`api/allBustime`);
-    return allTimes;
-
-}

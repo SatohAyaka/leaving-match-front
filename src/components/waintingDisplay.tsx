6311492c052a2d257cb9ed14fd8cab34ba4abb4a
SatohAyaka <sayaka310ss@docomo.ne.jp>
2025-10-13 14:10:31 +0900

    fix: 適応するクラスの変更

--- src/components/waintingDisplay.tsx
+++ src/components/waintingDisplay.tsx
@@ -3,7 +3,7 @@ export default function WaitingDisplay() {
     return (
         <div className={`display night`}>
             <div className="center-box">
-                <div className="message">取得中...</div>
+                <div className="time">取得中...</div>
             </div>
         </div>
     );

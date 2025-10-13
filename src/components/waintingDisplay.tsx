2815aa89ee8addceeee1449d6229043b701b7b5b
SatohAyaka <sayaka310ss@docomo.ne.jp>
2025-10-13 18:18:27 +0900

    fix: 待機画面の文字消去

--- src/components/waintingDisplay.tsx
+++ src/components/waintingDisplay.tsx
@@ -3,7 +3,6 @@ export default function WaitingDisplay() {
     return (
         <div className={`display night`}>
             <div className="center-box">
-                <div className="time">leaving-match</div>
             </div>
         </div>
     );
